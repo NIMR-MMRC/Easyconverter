1ba84d7b1f75ad2dee87cf557614e95fa1e9e9ac
Ayubu Masasi <ayubumasasi@gmail.com>
2018-02-07 07:54:18 +0300

    Variable names from a dataTable

--- EasyConverter.Model/DataSources/DataTableSource.cs
+++ EasyConverter.Model/DataSources/DataTableSource.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace EasyConverter.Model.DataSources
 {
     public class DataTableSource : IDataSource
     {
+
+
+        private System.Data.DataTable dataTable;
         public string GetDataLabel()
         {
             throw new NotImplementedException();
@@ -28,7 +32,7 @@ namespace EasyConverter.Model.DataSources
 
         public string[] GetVarnames()
         {
-            throw new NotImplementedException();
+           return (from System.Data.DataColumn f in dataTable.Columns select f.ColumnName ).ToArray() ;
         }
 
         public Dictionary<string, string> GetVarVal()
