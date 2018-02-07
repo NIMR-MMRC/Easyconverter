a5d888a30cfd106b7e59b9962b550409ee1d12d9
Ayubu Masasi <ayubumasasi@gmail.com>
2018-02-07 07:50:08 +0300

    Added a DataTableSource code

---
+++ EasyConverter.Model/DataSources/DataTableSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyConverter.Model.DataSources
+{
+    public class DataTableSource : IDataSource
+    {
+        public string GetDataLabel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProtoData GetDataTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, vlabs> GetValueLabels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetVarLabels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetVarnames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetVarVal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
