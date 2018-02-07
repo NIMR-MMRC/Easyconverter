b20e958953c8f1d3fdae7d3d14dbc1545e42cfec
Ayubu Masasi <ayubumasasi@gmail.com>
2018-02-07 07:51:09 +0300

    Rearrange the TabSeparatorDataSource to Datasources folder and namespace

--- EasyConverter.Model/DataSources/TabSeparatorDataSource.cs
+++ EasyConverter.Model/DataSources/TabSeparatorDataSource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace EasyConverter.Model
+namespace EasyConverter.Model.DataSources
 {
     public class TabSeparatorDataSource : IDataSource
     {
